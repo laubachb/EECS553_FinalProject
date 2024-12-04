ab1d61e63a8e61456eb21a93bf0308e009476e5b
Ben Laubach <61433046+laubachb@users.noreply.github.com>
2024-12-04 16:07:19 -0500

    Added files

---
+++ evaluation_position.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import average_precision_score
+from numpy.linalg import det
+
+
+def get_features(model, dataloader):
+    total = 0
+    model.eval()
+    
+    for batch_idx, (img, label) in enumerate(dataloader):
+        # do not pass to cuda 
+        # set up the initial batch
+        if batch_idx == 0:
+            f, _ = model(img)  # Forward pass
+            train_feature = f.detach().numpy()  # Convert to NumPy
+        # subsequent batch
+        else:
+            f, _ = model(img)
+            train_feature = np.concatenate((train_feature, f.detach().numpy()))
+    return train_feature
+
+
+def mahalanobis_distance(train_feature,e):
+    centerpoint = np.mean(train_feature , axis=0)  
+    p1 = e
+    p2 = centerpoint
+    # covariance matrix
+    covariance  = np.cov(train_feature, rowvar=False)
+    if det(covariance) != 0:
+        # covariance matrix power of -1
+        covariance_pm1 = np.linalg.matrix_power(covariance, -1)
+    else:
+        covariance_pm1 = np.linalg.pinv(covariance)
+    return (p1-p2).T.dot(covariance_pm1).dot(p1-p2)
+
+
+def evaluate_position(net,trainloader,validloader,testloader):
+    net.eval()  
+    train_feature= get_features(net,trainloader)
+    auroc_max = 0
+    with torch.no_grad():         
+        for batch_idx, (images, label) in enumerate(validloader):
+            if batch_idx == 0:
+                embedding,_ = net(images)
+                embedding = np.array(embedding)
+            else:
+                emb,_= net(images)
+                embedding = np.concatenate((embedding,np.array(emb)))
+        #print(embedding.shape)
+        distances = []
+        for e in embedding:
+            distance = mahalanobis_distance(train_feature,e)
+            distances.append(distance)
+        distances = np.array(distances)
+        
+        for batch_idx, (images, label) in enumerate(testloader):
+            if batch_idx == 0:
+                embedding,_ = net(images)
+                embedding = np.array(embedding)
+                labels = np.array(label)
+            else:
+                emb,_ = net(images)
+                embedding = np.concatenate((embedding,np.array(emb)))
+                labels = np.concatenate((labels,label))
+        #print(embedding.shape)
+        distances_test = []
+        for e in embedding:
+            distance_test = mahalanobis_distance(train_feature,e)
+            distances_test.append(distance_test)
+        distances_test = np.array(distances_test)
+
+        for percentile in range(85,86):
+            y_true = []
+            y_pred = []
+            total_correct = 0
+            #print("percentile:",percentile)
+            cutoff = np.percentile(distances,percentile)
+            pred = distances_test > cutoff
+            pred = pred.astype(int)
+            for i in labels:
+                y_true.append(i.item())
+            for i in pred:
+                y_pred.append(i.item())
+            pred = torch.tensor(pred)
+            
+            labels = torch.tensor(labels)
+            total_correct += torch.sum(pred == labels).item()
+
+            cm = confusion_matrix(y_true,y_pred)
+            accuracy = total_correct / len(testloader.dataset)
+            AUROC = roc_auc_score(y_true, distances_test)
+            PRAUC = average_precision_score(y_true, distances_test)
+        
+    return AUROC, PRAUC
--- saheart_analysis.py
+++ saheart_analysis.py
@@ -2,8 +2,10 @@ import numpy as np
 import torch
 import train
 import torch.optim as optimize
-from SemanticMask_Datasets import SMDataset_withClusters, SMDataset, SMDataset_Description
+from SemanticMask_Datasets import SMDataset_withClusters, SMDataset, SMDataset_Description, SMDataset_Position
 import evaluation
+import train_position
+import evaluation_position
 
 # Load in SA Heart Data from .npy files
 # Load in cluster assignments generated using the following files:
@@ -16,47 +18,41 @@ X_test = np.load('saheart_data/X_test_saheart.npy')
 y_test = np.load('saheart_data/y_test_saheart.npy')
 cluster_assignment = np.load('saheart_data/saheart_clusters.npy')
 cluster_assignment_description = np.load('saheart_data/saheart_clusters_description.npy')
-
+data_train = SMDataset(X_train, y_train)
+data_test = SMDataset(X_test, y_test)
+data_validation = SMDataset(X_valid, y_valid)
+train_dataset = torch.utils.data.DataLoader(dataset=data_train,batch_size=151)
+test_dataset = torch.utils.data.DataLoader(dataset=data_test,batch_size=75)
+validation_dataset = torch.utils.data.DataLoader(dataset=data_validation,batch_size=256)
 # Create torch dataset objects for train and test sets using the following files:
 # - SemanticMask_Datasets.py
-list_auroc = []
-list_pr_auc = []
-for pm in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    data_train_sm = SMDataset_withClusters(X_train, y_train, cluster_assignment, pm)
-    data_train = SMDataset(X_train, y_train, pm)
-    data_test = SMDataset(X_test, y_test, pm)
-    data_validation = SMDataset(X_valid, y_valid, pm)
+# list_auroc = []
+# list_pr_auc = []
+# for pm in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+#     data_train_sm = SMDataset_withClusters(X_train, y_train, cluster_assignment, pm)
+#     data_train = SMDataset(X_train, y_train, pm)
+#     data_test = SMDataset(X_test, y_test, pm)
+#     data_validation = SMDataset(X_valid, y_valid, pm)
 
-    # Load in datasets using torch
-    train_dataset = torch.utils.data.DataLoader(dataset=data_train,batch_size=151)
-    test_dataset = torch.utils.data.DataLoader(dataset=data_test,batch_size=75)
-    validation_dataset = torch.utils.data.DataLoader(dataset=data_validation,batch_size=256)
+#     # Load in datasets using torch
+#     train_dataset = torch.utils.data.DataLoader(dataset=data_train,batch_size=151)
+#     test_dataset = torch.utils.data.DataLoader(dataset=data_test,batch_size=75)
+#     validation_dataset = torch.utils.data.DataLoader(dataset=data_validation,batch_size=256)
 
-    # Fit NN model using the following files:
-    # - train.py
-    # - Loss.py
-
-    # Generate and fit model with standard SemanticMask
-    net = train.Encoder()
-    optimizer = optimize.Adam(net.parameters(), lr = 0.001)
-    trainloader_SemanticMask = torch.utils.data.DataLoader(data_train_sm, batch_size=151)  
-    net,training_loss = train.train_dnn(net,0.01,1000,optimizer,trainloader_SemanticMask)
-
-    auroc, pr_auc = evaluation.evaluate(net, train_dataset, validation_dataset, test_dataset)
-
-    # Generate and fit model with standard SemanticMask
-    net = train.Encoder()
-    optimizer = optimize.Adam(net.parameters(), lr = 0.001)
-    trainloader_SemanticMask = torch.utils.data.DataLoader(data_train_sm, batch_size=151)  
-    net,training_loss = train.train_dnn(net,0.01,1000,optimizer,trainloader_SemanticMask)
-    auroc, pr_auc = evaluation.evaluate(net, train_dataset, validation_dataset, test_dataset)
-    list_auroc.append(auroc)
-    list_pr_auc.append(pr_auc)
-    print("AUCROC(SemanticMask):", auroc)
-    print("PR-AUC(SemanticMask):", pr_auc)
-print("AUROC List: ", list_auroc)
-print("PRAUC List: ", list_pr_auc)
+#     # Generate and fit model with standard SemanticMask
+#     net = train.Encoder()
+#     optimizer = optimize.Adam(net.parameters(), lr = 0.001)
+#     trainloader_SemanticMask = torch.utils.data.DataLoader(data_train_sm, batch_size=151)  
+#     net,training_loss = train.train_dnn(net,0.01,1000,optimizer,trainloader_SemanticMask)
+#     auroc, pr_auc = evaluation.evaluate(net, train_dataset, validation_dataset, test_dataset)
+#     list_auroc.append(auroc)
+#     list_pr_auc.append(pr_auc)
+#     print("AUCROC(SemanticMask):", auroc)
+#     print("PR-AUC(SemanticMask):", pr_auc)
+# print("AUROC List: ", list_auroc)
+# print("PRAUC List: ", list_pr_auc)
 # Generate and fit model with SemanticMask+Description
+
 # data_train_desription = SMDataset_Description(X_train, y_train, cluster_assignment_description)
 # trainloader_description = torch.utils.data.DataLoader(data_train_desription, batch_size=151) 
 # net = train.Encoder()
@@ -66,3 +62,13 @@ print("PRAUC List: ", list_pr_auc)
 # auroc, pr_auc = evaluation.evaluate(net, train_dataset, validation_dataset, test_dataset)
 # print("AUCROC(SemanticMask+Description):", auroc)
 # print("PR-AUC(SemanticMask+Description):", pr_auc)
+
+data_train_position = SMDataset_Position(X_train, y_train, cluster_assignment_description)
+trainloader_position = torch.utils.data.DataLoader(data_train_position, batch_size=151) 
+net = train_position.Encoder()
+optimizer = optimize.Adam(net.parameters(), lr = 0.001)
+trainloader_SemanticMask_position = torch.utils.data.DataLoader(data_train_position, batch_size=151)  
+net,training_loss = train_position.train_dnn_position(net,0.01,1000,optimizer,trainloader_SemanticMask_position)
+auroc, pr_auc = evaluation_position.evaluate_position(net, train_dataset, validation_dataset, test_dataset)
+print("AUCROC(SemanticMask+Description):", auroc)
+print("PR-AUC(SemanticMask+Description):", pr_auc)
